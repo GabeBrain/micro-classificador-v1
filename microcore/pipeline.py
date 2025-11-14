5ae390332e2238b4e66bd7843fc0d220a2fff5b4
Gabriel Hxg <gabriel.gomes@brain.srv.br>
2025-11-14 15:00:18 -0300

    _LOJA_PREFIX_RE sem agencia

--- microcore/pipeline.py
+++ microcore/pipeline.py
@@ -8,7 +8,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from .utils import norm_text
 
 _LOJA_PREFIX_RE = re.compile(
-    r"^\s*(?:lojas?|ag[eê]ncias?|postos?|est[úu]dios?|empresas?)\s+(?:de|da|do|das|dos)?\s*",
+    r"^\s*(?:lojas?|postos?|est[úu]dios?|empresas?)\s+(?:de|da|do|das|dos)?\s*",
     re.IGNORECASE,
 )
 
